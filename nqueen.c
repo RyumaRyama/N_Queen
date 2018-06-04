59458ede15a7efebdf7f6255d66fd15ce8a63f84
Kohei Kaneshima <e165726@ie.u-ryukyu.ac.jp>
2018-06-04 19:08:35 +0900

    add c file

---
+++ nqueen.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+
+int main(int argc, char const* argv[])
+{
+    puts("hello, world!");
+    return 0;
+}
+
+/* Boardを出力 */
+void board_print() {
+    if (condition) {
+        
+    }else
+}
+
+/* 遺伝子生成 */
+void {
+    
+}
+
+/* 評価関数 */
+void calcFitness(int gene,int size) {
+    
+}
+
+/* 順列表現と順序表現へ変更 */
+void p_to_o(int gene,int size) {
+      
+}
+
+/* 順序表現から順列表現へ変換 */
+void o_to_p(int gene,int size) {
+    
+}
+
+/* 交叉*/
+/* 遺伝子のリストを渡すと交叉する */
+void cross(int_gene,) {
+    
+}
+
+
