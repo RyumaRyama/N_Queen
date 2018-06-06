c8daa1b5857406ce94b7f511689dfb15b017ab47
RyumaRyama <homes.ryu@gmail.com>
2018-06-06 09:51:52 +0900

    Made a struct list.

--- nqueen.c
+++ nqueen.c
@@ -1,46 +1,69 @@
 #include <stdio.h>
 struct gene_struct{
-    int[] *gene;
+    int *gene;
     int fitness;
-}
+};
 
 int main(int argc, char const* argv[])
 {
-    puts("hello, world!");
+    if(argc <= 1){
+        puts("Use '[FILE NAME] [QUEEN NUM]'.");
+        return 1;
+    }
+
+    int size, gene_num, n;
+
+    // Nの数
+    size = strtol(argv[1], NULL, 10);
+
+    //1世代あたりの遺伝子数
+    gene_num = 10;
+
+    //初期集団の生成
+    struct gene_struct gene_list[gene_num];
+    makeIniGene(gene_list, size);
+
+/*
+    //学習ループ
+    n = 1;
+    while(1){
+        
+    }
+*/
     return 0;
 }
 
-/* Boardを出力 */
+/*
+// Boardを出力 
 void board_print() {
     if (condition) {
         
     }else
 }
 
-/* 遺伝子生成 */
+// 遺伝子生成
 void {
     
 }
 
-/* 評価関数 */
+// 評価関数
 void calcFitness(int gene,int size) {
     
 }
 
-/* 順列表現と順序表現へ変更 */
+// 順列表現と順序表現へ変更
 void p_to_o(int gene,int size) {
       
 }
 
-/* 順序表現から順列表現へ変換 */
+// 順序表現から順列表現へ変換
 void o_to_p(int gene,int size) {
     
 }
 
-/* 交叉*/
-/* 遺伝子のリストを渡すと交叉する */
+// 交叉
+// 遺伝子のリストを渡すと交叉する
 void cross(int_gene,) {
     
 }
-
-
+*/
