ada8fbd73e12c2666ea58fed3b65da4809ea2144
Kohei Kaneshima <e165726@ie.u-ryukyu.ac.jp>
2018-06-13 21:41:40 +0900

    add sh file

---
+++ nqueen.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env sh
+
+if ls *.txt > /dev/null 2>&1 ;
+    then rm *.txt
+fi
+
+echo "C言語(最適化なし)実行速度計測結果(10回)" >> result_c.txt 
+echo "C言語(最適化あり)実行速度計測結果(10回)" >> result_c_opt.txt
+echo "Java言語実行速度計測結果(10回)" >> result_java.txt
+echo "Python言語実行速度計測結果(10回)" >> result_py.txt
+
+clang $1
+clang -o Prime_opt -O $1
+javac $2
+
+for i in `seq 1 10`
+do   
+    (time -p ./a.out $4)  | grep -e "real" >> result_c.txt 2>&1 
+    echo "------------" >> result_c.txt
+    (time -p ./Prime_opt $4) >> result_c_opt.txt 2>&1 
+    echo "------------" >> result_c_opt.txt
+    (time -p java NQueen $4) >> result_java.txt 2>&1 
+    echo "------------" >> result_java.txt
+    (time -p python3 $3 $4) >> result_py.txt 2>&1 
+    echo "------------" >> result_py.txt 
+done
